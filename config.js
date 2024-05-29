c4667f3e1dfa0f5022cb48ee44983460059358db
khanbhaig2 <rubykennedy@safrica.com>
2024-05-29 16:37:09 +0500

    Update config.js

--- config.js
+++ config.js
@@ -20,8 +20,8 @@ global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
 global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
 global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643";
 global.website=process.env.GURL || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643" ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://github.com/SuhailTechInfo/Suhail-Md/blob/main/lib/assets/suhail.jpg?raw=true" ; // SET LOGO FOR IMAGE 
-global.caption = process.env.CAPTION || global.caption || "𝐶𝑟𝑒𝑎𝑡𝑒𝑑 𝐵𝑦 𝑌𝑎𝑠𝑖𝑟" 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.postimg.cc/SN3Yfzjy/suhail.jpg?raw=true" ; // SET LOGO FOR IMAGE 
+global.caption = process.env.CAPTION || global.caption || "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʏᴀꜱɪʀ ᴋʜᴀɴ" 
 
 
 global.devs = "923184474176" // Developer Contact
